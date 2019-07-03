e8055f57f09f020872a1f7dd3d6324c119b2281c
SMakaiTakori <makait87@icloud.com>
2019-07-03 16:05:05 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    var input = document.querySelector('.numberInput');
+    var para = document.querySelector('p');
+
+    function squared (num){
+        return num * num;
+    }
+
+    function cubed(num){
+        return num*num*num;
+    }
+    function factorial(num){
+        var x = num;
+        while(x>1){
+            numb *= x-1;
+            x--;
+        }
+        return num;
+    }
+    
+
+
