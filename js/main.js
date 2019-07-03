101540b113cc4a4ae99366bc0e5056b48d72e8ca
SMakaiTakori <makait87@icloud.com>
2019-07-03 16:15:26 -0400

    added event handler and conditional test to print when a number or non number is entered into the field

--- js/main.js
+++ js/main.js
@@ -11,10 +11,22 @@
     function factorial(num){
         var x = num;
         while(x>1){
-            numb *= x-1;
+            num *= x-1;
             x--;
         }
         return num;
+    }
+
+    input.onchange = function(){
+        var num = input.value;
+
+        if (isNaN(num)){
+            para.textContent = 'You need to enter a number!';
+        }else {
+            para.textContent =  num + ' squared is ' + squared(num) + '. ' +
+                                num + ' cubed is ' + cubed(num) + '. ' +
+                                num + ' factorial is ' + factorial(num) + '.'; 
+        }
     }
     
 
